d536dd3236f7fc2241a8ec2044762885ef07c76e
tvkomal <komal.mandkriya@techvalens.com>
2026-07-07 17:46:48 +0530

    Deployement rediness

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,9 +4,15 @@ import swaggerSpec from "./config/swagger.js";
 import errorHandler from "./middleware/errorHandler.js";
 import authRoutes from "./routes/auth.routes.js";
 import healthRoutes from "./routes/health.routes.js";
-
+import cors from "cors";
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  }),
+);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/health", healthRoutes);
 app.use("/api/auth", authRoutes);
